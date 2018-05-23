071cc0b6d1d46c6317f59d1470d347a52d0c9101
Jeff Standridge <standridgejeff@gmail.com>
2018-05-23 13:26:55 -0500

    Scaffolding Models and Controllers, and making needed changes to Views as they are added

---
+++ TrueStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using TrueStore.Models;
+
+namespace TrueStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            var category = new List<Category>
+            {
+                new Category {Name = "Electronics"},
+                new Category {Name = "Furniture"},
+                new Category {Name = "Accessories"},
+            };
+            return View(category);
+        }
+
+        public ActionResult Browse(string category)
+        {
+            var categoryModel = new Category { Name = category };
+            return View(categoryModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var Item = new Item { Title = "Item" + id };
+            return View(Item);
+        }
+    }
+}
---
+++ TrueStore/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrueStore.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public int ProducerId { get; set; }
+        public string Price { get; set; }
+        public string ItemArtUrl { get; set; }
+        public Category Category { get; set; }
+        public Producer Producer { get; set; }
+    }
+}
